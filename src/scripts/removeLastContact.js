4672c5037b515d3d92efe3ab45853041dbb7b26c
Anastasia-Mialitsina <malicynaanastasia@gmail.com>
2025-05-06 14:06:18 +0300

    all

--- src/scripts/removeLastContact.js
+++ src/scripts/removeLastContact.js
@@ -1,3 +1,16 @@
-export const removeLastContact = async () => {};
+import { readContacts } from '../utils/readContacts.js';
+import { writeContacts } from '../utils/writeContacts.js';
+
+const removeLastContact = async () => {
+  const contacts = await readContacts();
+
+  if (contacts.length > 0) {
+    contacts.pop(); 
+    await writeContacts(contacts); 
+    console.log('Last contact has been removed!');
+  } else {
+    console.log('No contacts to remove.');
+  }
+};
 
 removeLastContact();
